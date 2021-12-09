dfa51dab77638aa787248ccd712064aed58f9a69
CarlosM <karlos_m96@hotmail.com>
2021-12-08 21:01:57 -0500

    creacion de app

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    ##return render(request, 'polls/index.html')
+    return HttpResponse("Welcome to my page, you are in polls page.")
+
+def details(request, poll_id):
+    return HttpResponse("You are looking at poll %s." % poll_id)
+
+
+def results(request, poll_id):
+    return HttpResponse("You are looking at the result of poll %s." % poll_id)
